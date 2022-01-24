8f69276ba7c467121e6d4b1b3f85a7713e005667
Zhentian Wan <zhentian.wan@varian.com>
2022-01-24 14:11:15 +0200

    Using arbitrary values

--- pages/servers/[sid].js
+++ pages/servers/[sid].js
@@ -1,14 +1,20 @@
 import Post from "../Post";
 import { useRouter } from "next/router";
+import { VerifiedIcon, CheckIcon, ChevronIcon } from "../../components/icon";
 
 export default function Server1() {
   const router = useRouter();
   return (
     <>
       <div className="bg-gray-800 w-60 flex flex-col">
-        <div className="px-3 h-12 flex items-center shadow-md font-title text-white">
+        <button className="px-4 h-12 flex items-center shadow-sm font-title text-white text-[15px] transition hover:bg-gray-550/[0.16]">
+          <div className="relative w-4 h-4 mr-1">
+            <VerifiedIcon className="absolute w-4 h-4 text-gray-550" />
+            <CheckIcon className="absolute w-4 h-4" />
+          </div>
           Server {router.query.sid}
-        </div>
+          <ChevronIcon className="w-[18px] h-[18px] ml-auto opacity-80" />
+        </button>
         <div className="p-3 text-gray-300 flex-1 overflow-y-scroll space-y-2 font-medium">
           <div className="flex-1 p-4 text-white">channels (unread)</div>
         </div>
